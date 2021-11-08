9d4fc9e1a8d6879ad5808c7afe2594d7a645e6f0
script000kiddies000 <scriptkiddies@protonmail.com>
2021-11-09 00:23:58 +0700

    fixed asset

---
+++ js/adinusa.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+    $('.content-course > ul').addClass('list-disc ml-5 space-y-1 mt-3');
+    $('.content-course > ol').addClass('list-decimal ml-5 space-y-1 mt-3');
+});
