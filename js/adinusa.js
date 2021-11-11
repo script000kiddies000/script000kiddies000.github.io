43128be3538616d987e70385deff7dea3d188264
script000kiddies000 <scriptkiddies@protonmail.com>
2021-11-11 19:58:28 +0700

    js force https

--- js/adinusa.js
+++ js/adinusa.js
@@ -1,3 +1,8 @@
+if (location.protocol !== "https:") {
+    location.protocol = "https:";
+}
+  
+
 $( document ).ready(function() {
     $('.content-course > ul').addClass('list-disc ml-5 space-y-1 mt-3');
     $('.content-course > ol').addClass('list-decimal ml-5 space-y-1 mt-3');
